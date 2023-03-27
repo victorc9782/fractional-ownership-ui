aa01a1a7c6000de2ee10b32d659be5644039eb4c
C.Victor <c>
2023-03-27 13:53:58 +0800

    Update transpilePackages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['gsap']
 }
 
 module.exports = nextConfig
-const withTM = require('next-transpile-modules')(['gsap']);
-module.exports = withTM();
+// const withTM = require('next-transpile-modules')(['gsap']);
+// module.exports = withTM();
