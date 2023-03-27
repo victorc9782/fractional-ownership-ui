573b1d3d4b32d946c57553222346ab999d9d2848
C.Victor <c>
2023-03-27 18:54:38 +0800

    Add buy and sell with smart contract

--- src/pages/trade/[contractAddress].js
+++ src/pages/trade/[contractAddress].js
@@ -1,6 +1,8 @@
-import { useState, useEffect } from "react";
-import { useAccount, useDisconnect } from "wagmi";
+import { useState, useEffect, useRef } from "react";
+import { useAccount, useDisconnect, useSigner } from "wagmi";
 import { useRouter } from "next/router";
+import { Input } from "@nextui-org/react";
+
 const ethers = require('ethers')
 
 import FRACTION_OWNERSHIP_ABI from "../../../contracts/FractionalOwnership.json";
@@ -9,6 +11,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 export default function Trade() {
     const { isConnected, address } = useAccount();
+    const { data: signer, isError } = useSigner()
     const router = useRouter();
     const { contractAddress } = router.query;
 
@@ -19,26 +22,83 @@ export default function Trade() {
     const [propertiesTotalShares, setPropertiesTotalShares] = useState(0);
     const [propertiesSharePrice, setPropertiesSharePrice] = useState(0);
     const [propertiesRemainingShares, setPropertiesRemainingShares] = useState(0);
+    const [propertiesOwningShares, setPropertiesOwningShares] = useState(0);
+    
+    const [amountInput, setAmountInput] = useState(0);
+    const amountRef =  useRef(null);
 
     const init = async () => {
         try {
-          const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URLL);
-          const fractionalOwnershipContract = new ethers.Contract(contractAddress, FRACTION_OWNERSHIP_ABI.abi, provider);
-    
-          const propertiesInfo = await fractionalOwnershipContract.getInfo();
-          setIsContractValid(true);
+            const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+            const fractionalOwnershipContract = new ethers.Contract(contractAddress, FRACTION_OWNERSHIP_ABI.abi, provider);
+        
+            const propertiesInfo = await fractionalOwnershipContract.getInfo();
+            setIsContractValid(true);
 
-          setPropertiesName(propertiesInfo.name)
-          setPropertiesDescription(propertiesInfo.description)
-          setPropertiesTotalShares(propertiesInfo.totalShares.toNumber())
-          setPropertiesSharePrice(propertiesInfo.sharePrice.toNumber())
-          setPropertiesRemainingShares(propertiesInfo.remainingShares.toNumber())
+            setPropertiesName(propertiesInfo.name)
+            setPropertiesDescription(propertiesInfo.description)
+            setPropertiesTotalShares(propertiesInfo.totalShares.toNumber())
+            setPropertiesSharePrice(propertiesInfo.sharePrice.toNumber())
+            setPropertiesRemainingShares(propertiesInfo.remainingShares.toNumber())
+
+            const owningShares = await fractionalOwnershipContract.getOwningShares(address);
+            setPropertiesOwningShares(owningShares.toNumber())
+
+            setAmountInput(0);
         } catch (err) {
           console.error(err);
         } finally {
           setIsLoading(false);
         }
     };
+
+    const buyShares = async () => {
+        try {
+            if (amountInput <= 0 || amountInput > propertiesRemainingShares){
+                alert("Invalid shares to buy")
+                throw new Error("Invalid shares to buy")
+            }
+            const fractionalOwnershipContract = new ethers.Contract(contractAddress, FRACTION_OWNERSHIP_ABI.abi, signer);
+
+            const tx = await fractionalOwnershipContract.buyShares(amountInput, {value: amountInput*propertiesSharePrice});
+            const response = await tx.wait();
+
+            console.log(response)
+        } catch (err) {
+            console.error(err);
+        } finally {
+            init();
+        }
+    }
+    
+    const sellShares = async () => {
+        try {
+            if (amountInput <= 0 || amountInput > propertiesOwningShares){
+                alert("Invalid shares to sell")
+                throw new Error("Invalid shares to sell")
+            }
+            const fractionalOwnershipContract = new ethers.Contract(contractAddress, FRACTION_OWNERSHIP_ABI.abi, signer);
+
+            const tx = await fractionalOwnershipContract.sellShares(amountInput);
+            const response = await tx.wait();
+
+            console.log(response)
+        } catch (err) {
+            console.error(err);
+        } finally {
+            init();
+        }
+    }
+
+    const onChangeAmountPicker = (e) => {
+        const newAmount = e.target.valueAsNumber;
+        if(newAmount >= 0 || newAmount == ""){
+            setAmountInput(newAmount)
+            if(newAmount > propertiesRemainingShares || newAmount == ""){
+                setAmountInput(propertiesRemainingShares)
+            }
+        }
+    }
 
     useEffect(() => {
         if (router.isReady ) {
@@ -62,9 +122,16 @@ export default function Trade() {
                     totalShares: {propertiesTotalShares} <br/>
                     sharePrice: {propertiesSharePrice} <br/>
                     remainingShares: {propertiesRemainingShares} <br/>
+                    propertiesOwningShares: {propertiesOwningShares} <br/>
                     <div class="btn-group" role="group" aria-label="Basic example">
-                        <button type="button" class="btn btn-outline-success">Buy</button>
-                        <button type="button" class="btn btn-outline-danger">Sell</button>
+                        <Input 
+                            type="number"
+                            required
+                            onChange={onChangeAmountPicker}
+                            value={amountInput}
+                        />
+                        <button type="button" class="btn btn-outline-success" onClick={buyShares}>Buy</button>
+                        <button type="button" class="btn btn-outline-danger" onClick={sellShares}>Sell</button>
                     </div>
                 </div>}
             </div>
