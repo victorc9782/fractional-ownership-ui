283dc29b1daab68f2ad241531c0dd4aaf1f7a065
C.Victor <victorchu2002@gmail.com>
2023-04-01 16:19:05 +0800

    Enhance trade page ui

--- src/pages/trade/[contractAddress].js
+++ src/pages/trade/[contractAddress].js
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from "react";
+import { Card, Text, Row, Col, Button, Progress, Loading } from '@nextui-org/react';
 import { useAccount, useDisconnect, useSigner, configureChains } from "wagmi";
 import { useRouter } from "next/router";
 import { Input } from "@nextui-org/react";
@@ -24,6 +25,7 @@ export default function Trade() {
     const [propertiesRemainingShares, setPropertiesRemainingShares] = useState(0);
     const [propertiesOwningShares, setPropertiesOwningShares] = useState(0);
     const [approvedBuy, setApprovedBuy] = useState(false);
+    const [isTrading, setIsTrading] = useState(false);
     
     const [amountInput, setAmountInput] = useState(0);
 
@@ -38,6 +40,7 @@ export default function Trade() {
 
     const init = async () => {
         try {
+            console.log("init called")
             setIsLoading(true);
             const propertiesInfo = await getTradeInfo(address)
         
@@ -55,12 +58,14 @@ export default function Trade() {
         } catch (err) {
           console.error(err);
         } finally {
-          setIsLoading(false);
+            setIsTrading(false);
+            setIsLoading(false);
         }
     };
 
     const buyShares = async () => {
         try {
+            setIsTrading(true);
             if (amountInput <= 0 || amountInput > propertiesRemainingShares){
                 alert("Invalid shares to buy")
                 throw new Error("Invalid shares to buy")
@@ -85,6 +90,7 @@ export default function Trade() {
     
     const sellShares = async () => {
         try {
+            setIsTrading(true);
             if (amountInput <= 0 || amountInput > propertiesOwningShares){
                 alert("Invalid shares to sell")
                 throw new Error("Invalid shares to sell")
@@ -129,7 +135,7 @@ export default function Trade() {
 
     return (
         <>
-            <div className="container text-center">
+            <div className="container">
                 {isConnected && isLoading && <div>Loading...</div>}
                 {isConnected && !isLoading && !isContractValid && <div>Invalid properties<br/></div>}
                 {isConnected && !isLoading && isContractValid && 
@@ -140,23 +146,104 @@ export default function Trade() {
                             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={() => setIsShowTransactionSuccessAlert(false)}></button>
                         </div>
                     }
-                    Name: {propertiesName} <br/>
-                    Description: {propertiesDescription} <br/>
-                    totalShares: {propertiesTotalShares} <br/>
-                    sharePrice: {propertiesSharePrice} <br/>
-                    remainingShares: {propertiesRemainingShares} <br/>
-                    propertiesOwningShares: {propertiesOwningShares} <br/>
-                    <div class="btn-group" role="group" aria-label="Basic example">
-                        <Input 
-                            type="number"
-                            required
-                            onChange={onChangeAmountPicker}
-                            value={amountInput}
-                            disabled={!approvedBuy}
-                        />
-                        <button type="button" class="btn btn-outline-success" onClick={buyShares} disabled={!approvedBuy}>Buy</button>
-                        <button type="button" class="btn btn-outline-danger" onClick={sellShares} disabled={!approvedBuy}>Sell</button>
-                    </div>
+
+                    <Card css={{ w: "100%", h: "600px" }}>
+                        <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+                            <Col>
+                                <Text size={12} weight="bold" transform="uppercase" color="#9E9E9E">
+                                    New
+                                </Text>
+                                <Text h3 color="white">
+                                    {propertiesName}
+                                </Text>
+                            </Col>
+                        </Card.Header>
+                        <Card.Body css={{ p: 0 }}>
+                            <Card.Image
+                                src="/house.jpeg"
+                                objectFit="cover"
+                                width="100%"
+                                height="100%"
+                                alt="A Sweethouse"
+                            />
+                        </Card.Body>
+                        <Card.Footer
+                            isBlurred
+                            css={{
+                                position: "absolute",
+                                bgBlur: "#0f111466",
+                                borderTop: "$borderWeights$light solid $gray800",
+                                bottom: 0,
+                                zIndex: 1,
+                            }}
+                            >
+                            <Row>
+                                <Col>
+                                    <Row>
+                                        <Col>
+                                            <Text color="#d1d1d1" size={12}>
+                                                {propertiesDescription}
+                                            </Text>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col>
+                                            <Text color="white" size={12}>
+                                                ${propertiesSharePrice}@1
+                                            </Text>
+                                        </Col>
+                                    </Row>
+                                </Col>
+                                <Col>
+                                    <Row justify="center">
+                                        <Col>
+                                                <Progress value={propertiesRemainingShares/propertiesTotalShares*100} color="primary" />
+                                        </Col>
+                                    </Row>
+                                    <Row justify="center">
+                                        <Text color="white" size={12}>
+                                            {propertiesRemainingShares}/{propertiesTotalShares}
+                                        </Text>
+                                    </Row>
+
+                                </Col>
+                                <Col>
+                                    <Row justify="flex-end" alignContent="center">
+                                    {isTrading ?
+                                        <Button disabled auto bordered color="primary" css={{ px: "$13" }}>
+                                            <Loading color="currentColor" size="sm" />
+                                        </Button>
+                                    :
+                                        <Button.Group rounded>
+                                            <Input 
+                                                type="number"
+                                                required
+                                                onChange={onChangeAmountPicker}
+                                                value={amountInput}
+                                                disabled={!approvedBuy}
+                                                css={{ $$inputColor: "#ffffff50"}}
+                                            />
+                                            <Button 
+                                                css={{ color: "#94f9f0", bg: "#94f9f026" }}
+                                                onPress={buyShares}
+                                                disabled={!approvedBuy}
+                                            >
+                                                Buy
+                                            </Button>
+                                            <Button
+                                                css={{color:"#de4343", bg: "#de434326"}}
+                                                onPress={sellShares}
+                                                disabled={!approvedBuy}
+                                            >
+                                                Sell
+                                            </Button>
+                                        </Button.Group>
+                                    }
+                                    </Row>
+                                </Col>
+                            </Row>
+                        </Card.Footer>
+                    </Card>
                 </div>}
             </div>
         </>
